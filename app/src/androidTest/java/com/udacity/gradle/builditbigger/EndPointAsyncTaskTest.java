a5c0818ca0ba31ac876e4a131da6aa50ff8b68ba
Mahmoud-AbdelFatah <abdelfatahm3@gmail.com>
2017-08-11 10:39:21 +0200

    initial commit

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointAsyncTaskTest.java
@@ -0,0 +1,22 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Mahmoud on 8/11/2017.
+ */
+@RunWith(AndroidJUnit4.class)
+public class EndPointAsyncTaskTest {
+    @Test
+    public void testDoInBackground() throws Exception {
+        com.udacity.gradle.builditbigger.MainActivityFragment fragment = new com.udacity.gradle.builditbigger.MainActivityFragment();
+        fragment.testFlag = true;
+        new EndpointAsyncTask().execute(fragment);
+        Thread.sleep(5000);
+        assertTrue("error= " + fragment.loadedJoke, fragment.loadedJoke != null);
+    }
+}
